05af2b0a72c06c176b4b96c6d959c2d324f35fdd
Heuler Silva <heulersilva@yahoo.com.br>
2026-03-18 16:50:46 -0300

    Começando a colocar peças no tabuleiro

--- XadrezConsole/Tabuleiro/Posicao.cs
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -2,7 +2,7 @@
 
 namespace tabuleiro
 {
-    internal class Posicao {
+    class Posicao {
       public int linha { get; set; }
       public int coluna { get; set; }
 
---
+++ XadrezConsole/xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor)
+            : base(tab, cor)
+        { 
+        }
+        override public string ToString()
+        {
+            return "R";
+        }
+    }
+}
