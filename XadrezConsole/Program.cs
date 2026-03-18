039e2c338d239edf5de3d11d4a4f70be173f562a
Heuler Silva <heulersilva@yahoo.com.br>
2026-03-18 15:52:12 -0300

    Criando classe posiçao

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+
+namespace XadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.AccessControl;
+
+namespace Tabuleiro
+{
+    internal class Posicao {
+      public int linha { get; set; }
+      public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna) { 
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+        override public string ToString()
+        {
+            return linha 
+                + ", " 
+                + coluna;
+        }
+    }
+}
